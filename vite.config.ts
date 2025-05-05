5c76d9c7d59782f641bad1591dd7da1b4efcbb54
Gaurav Narula <gaurav.narula@1mg.com>
2025-05-05 20:13:39 +0530

    New Repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure this is 'dist' or the directory you've configured in Netlify
+    // ... other build options
+  },
 })
