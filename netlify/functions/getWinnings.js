1a07816a8bb75c12c5b15f63d600b5490eac49e9
Gaurav Narula <gaurav.narula@1mg.com>
2025-07-28 07:03:44 +0530

    Neon DB

--- netlify/functions/getWinnings.js
+++ netlify/functions/getWinnings.js
@@ -54,11 +54,8 @@ export default async (req) => {
 
         const allRecords = await sql`
             SELECT phone_hash, game_type, winnings, timestamp FROM winnings
-            WHERE phone_hash IN (${sql(phoneHashes)})
+            WHERE phone_hash IN (${phoneHashes.join(',')})
         `;
-        console.log(allRecords);
-        console.log(`SELECT phone_hash, game_type, winnings, timestamp FROM winnings
-            WHERE phone_hash IN (${await sql(phoneHashes)})`)
         const teenPattiData = processDataForChart(allRecords, phoneHashes, 'teen-patti');
         const pokerData = processDataForChart(allRecords, phoneHashes, 'poker');
 
